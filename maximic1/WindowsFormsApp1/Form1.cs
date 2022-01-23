4226ec216d12bf868fc747d06ad1d77534014308
maximic1 <74239130+maximic1@users.noreply.github.com>
2022-01-23 22:43:09 +0300

    Add files via upload

---
+++ maximic1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Button helloButton = new Button();
+            helloButton.BackColor = Color.LightGray;
+            helloButton.ForeColor = Color.DarkGray;
+            helloButton.Location = new Point(10, 10);
+            helloButton.Text = "Привет";
+            this.Controls.Add(helloButton);
+        }
+
+    }
+}
